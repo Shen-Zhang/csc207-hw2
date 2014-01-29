5a70a5efbf6274e4c8fb8b38165448171ef73a0b
Shen-Zhang <zhangshe@grinnell.edu>
2014-01-29 14:11:14 -0600

    class Rational modified

--- hw2/src/edu/grinnell/csc207/zhangshe/hw2/TwoC.java
+++ hw2/src/edu/grinnell/csc207/zhangshe/hw2/TwoC.java
@@ -1,140 +1,155 @@
 package edu.grinnell.csc207.zhangshe.hw2;
 
-public class TwoC {
-	  
-	  /**
-	   * Compare two rational numbers, return the greater one. (Observer)
-	   */
-	  
-	  public Rational compare (Rational num1, Rational num2)
-	  {
-		  return null; // STUB
-	  } // compare (Rational num1, Rational num2)
-	  
-	  
-	  /** 
-	   * Increment the given rational number. (Mutator)
-	   * 
-	   */
-	  public Rational increment (Rational num)
-	  {
-		  return null;  // STUB
-	  } // increment (Rational num)
-	  
-	  /**
-	   * Decrement the given rational number. (Mutator)
-	   */
-	  
-	  public Rational decrement (Rational num)
-	  {
-		  return null; // STUB
-	  } // decrement (Rational num)
-	  
-	  /**
-	   * Determine if the rational number is positive or not (Observer)
-	   */
-	  
-	  public boolean isPositive (Rational num)
-	  {
-		  return false;  // STUB
-	  } // isPositive (Rational num)
-	  
-	  
-	  /**
-	   * Determine if the rational number is negative or not (Observer)
-	   */
-	  
-	  public boolean isNegative (Rational num)
-	  {
-		  return false; // STUB
-	  } // isNegative (Rational num)
-	  /**
-	   * The rational number raised to the power of an integer (Observer/Mutator)
-	   */
-	  
-	  public Rational expt (Rational num, int n)
-	  {
-		  return null;  // STUB
-	  } // expt (Rational num, int n)
-	  
-	  
-	  /**
-	   * Get the sqrtroot of the rational number (if the square root exists)   (Observer/Constructor)
-	   */
-	  public Rational sqrtroot (Rational num)
-	  {
-		  return null; //STUB
-	  } // sqrtroot (Rational num)
-	  
-	  
-	  /**
-	   * Determine if the rational number is an integer (Observer)
-	   */
-	  public boolean isInteger (Rational num)
-	  {
-		  return false; //STUB
-	  } // isInteger (Rational num)
-	  
-	  /**
-	   * 
-	   * Get the average of two rational numbers (Observer/Constructor)
-	   */
-	  public Rational average (Rational num1, Rational num2)
-	  {
-		  return null; //STUB
-	  } // average (Rational num)
-	
-	  
-	  /**
-	   * Construct a rational number by two integers  (Constructor)
-	   */
-	  
-	  public Rational ConstructRational (int a, int b)
-	  {
-		  return null; // STUB
-	  } // ConstructRational (int a, int b)
-	  
-	  
-	  /**
-	   *  Determine if two rational numbers are equal to each other  (Observer)
-	   */
-	  
-	  public boolean equalTo (Rational num1, Rational num2)
-	  {
-		  return false; // STUB
-	  } // equalTo (Rational num1, Rational num2)
-	  
-	  /**
-	   * Take two rational numbers and subtract the first rational number from the second one. The output is a rational number. (Observer/Constructor)
-	   */
-	  
-	  public Rational difference (Rational num1, Rational num2)
-	  {
-		  return null; // STUB
-	  } // difference (Rational num1, Rational num2)
+public class TwoC
+{
 
-	  
-	  /**
-	   * Take an array of rational numbers and sort them from largerst to smallest (Mutator)
-	   */
-	  public Rational sort (Rational[] nums)
-	  {
-	    return null; // STUB
-	  } // sort (Rational[] nums)
-	  
-	  /**
-	   * Take two arrays of rational numbers and merge them into a new array (Observer/Constructor)
-	   */
-	  public Rational merge (Rational[] nums1, Rational[] nums2)
-	  {
-	    return null; // STUB
-	  } // merge (Rational[] nums)
-	  
-	  /**
-	   * Compare two arrays of rational numbers, return true if they are identical  (Observer)
-	   */
-	  public boolean compareArr (Rational[] nums1, Rational[] nums2)
-	  {
-	    return false; // STUB
-	  } // compareArr (Rational[] nums1, Rational[] nums2)
+  /**
+   * Compare two rational numbers, return the greater one. (Observer)
+   */
+
+  public Rational
+    compare (Rational num1, Rational num2)
+  {
+    return null; // STUB
+  } // compare (Rational num1, Rational num2)
+
+  /**
+   * Increment the given rational number. (Mutator)
+   * 
+   */
+  public Rational
+    increment (Rational num)
+  {
+    return null; // STUB
+  } // increment (Rational num)
+
+  /**
+   * Decrement the given rational number. (Mutator)
+   */
+
+  public Rational
+    decrement (Rational num)
+  {
+    return null; // STUB
+  } // decrement (Rational num)
+
+  /**
+   * Determine if the rational number is positive or not (Observer)
+   */
+
+  public boolean
+    isPositive (Rational num)
+  {
+    return false; // STUB
+  } // isPositive (Rational num)
+
+  /**
+   * Determine if the rational number is negative or not (Observer)
+   */
+
+  public boolean
+    isNegative (Rational num)
+  {
+    return false; // STUB
+  } // isNegative (Rational num)
+
+  /**
+   * The rational number raised to the power of an integer (Observer/Mutator)
+   */
+
+  public Rational
+    expt (Rational num, int n)
+  {
+    return null; // STUB
+  } // expt (Rational num, int n)
+
+  /**
+   * Get the sqrtroot of the rational number (if the square root exists)
+   * (Observer/Constructor)
+   */
+  public Rational
+    sqrtroot (Rational num)
+  {
+    return null; // STUB
+  } // sqrtroot (Rational num)
+
+  /**
+   * Determine if the rational number is an integer (Observer)
+   */
+  public boolean
+    isInteger (Rational num)
+  {
+    return false; // STUB
+  } // isInteger (Rational num)
+
+  /**
+   * 
+   * Get the average of two rational numbers (Observer/Constructor)
+   */
+  public Rational
+    average (Rational num1, Rational num2)
+  {
+    return null; // STUB
+  } // average (Rational num)
+
+  /**
+   * Construct a rational number by two integers (Constructor)
+   */
+
+  public Rational
+    ConstructRational (int a, int b)
+  {
+    return null; // STUB
+  } // ConstructRational (int a, int b)
+
+  /**
+   * Determine if two rational numbers are equal to each other (Observer)
+   */
+
+  public boolean
+    equalTo (Rational num1, Rational num2)
+  {
+    return false; // STUB
+  } // equalTo (Rational num1, Rational num2)
+
+  /**
+   * Take two rational numbers and subtract the first rational number from the
+   * second one. The output is a rational number. (Observer/Constructor)
+   */
+
+  public Rational
+    difference (Rational num1, Rational num2)
+  {
+    return null; // STUB
+  } // difference (Rational num1, Rational num2)
+
+  /**
+   * Take an array of rational numbers and sort them from largerst to smallest
+   * (Mutator)
+   */
+  public Rational
+    sort (Rational[] nums)
+  {
+    return null; // STUB
+  } // sort (Rational[] nums)
+
+  /**
+   * Take two arrays of rational numbers and merge them into a new array
+   * (Observer/Constructor)
+   */
+  public Rational
+    merge (Rational[] nums1, Rational[] nums2)
+  {
+    return null; // STUB
+  } // merge (Rational[] nums)
+
+  /**
+   * Compare two arrays of rational numbers, return true if they are identical
+   * (Observer)
+   */
+  public boolean
+    compareArr (Rational[] nums1, Rational[] nums2)
+  {
+    return false; // STUB
+  } // compareArr (Rational[] nums1, Rational[] nums2)
 }
